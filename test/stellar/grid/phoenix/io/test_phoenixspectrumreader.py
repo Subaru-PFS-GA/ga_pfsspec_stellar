b93d92c839927f4b3a3c8eba2bb711b3ca900d5b
Laszlo Dobos <dobos@jhu.edu>
2022-06-06 22:44:42 -0400

    Tests updated.

--- test/stellar/grid/phoenix/io/test_phoenixspectrumreader.py
+++ test/stellar/grid/phoenix/io/test_phoenixspectrumreader.py
@@ -7,7 +7,7 @@ from pfsspec.stellar.grid.phoenix.io import PhoenixSpectrumReader
 
 class TestPhoenixSpectrumReader(TestBase):
     def test_read(self):
-        filename = os.path.join(self.PFSSPEC_DATA_PATH, 'models/stellar/grid/phoenix/lte11600-2.00-0.0.PHOENIX-ACES-AGSS-COND-2011-HiRes.fits')
+        filename = os.path.join(self.PFSSPEC_DATA_PATH, 'download/models/stellar/grid/phoenix/phoenix_HiRes/Z-0.0/lte11600-2.00-0.0.PHOENIX-ACES-AGSS-COND-2011-HiRes.fits')
 
         r = PhoenixSpectrumReader(filename)
         spec = r.read()
@@ -18,8 +18,8 @@ class TestPhoenixSpectrumReader(TestBase):
         self.assertEqual(spec.wave.shape, spec.flux.shape)
 
     def test_get_filename(self):
-        fn = PhoenixSpectrumReader.get_filename(M_H=-2, T_eff=9600, log_g=4)
-        self.assertEqual('lte09600-4.00-2.0.PHOENIX-ACES-AGSS-COND-2011-HiRes.fits', fn)
+        fn = PhoenixSpectrumReader().get_filename(M_H=-2, T_eff=9600, log_g=4, a_M=0.0)
+        self.assertEqual('Z-2.0/lte09600-4.00-2.0.PHOENIX-ACES-AGSS-COND-2011-HiRes.fits', fn)
 
     def test_parse_filename(self):
         fn = 'lte09600-4.00-2.0.PHOENIX-ACES-AGSS-COND-2011-HiRes.fits'
