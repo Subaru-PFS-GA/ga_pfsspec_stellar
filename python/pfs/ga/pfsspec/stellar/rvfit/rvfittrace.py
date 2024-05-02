b02aed8785961a007b49727647894682c23ede29
Laszlo Dobos <dobos@jhu.edu>
2024-05-02 13:10:50 -0400

    Improvements to spectrum plots.

--- python/pfs/ga/pfsspec/stellar/rvfit/rvfittrace.py
+++ python/pfs/ga/pfsspec/stellar/rvfit/rvfittrace.py
@@ -1,7 +1,7 @@
 import os
 import numpy as np
 
-from pfs.ga.pfsspec.core.plotting import SpectrumPlot, styles
+from pfs.ga.pfsspec.core.plotting import SpectrumPlot, PriorPlot, styles
 from pfs.ga.pfsspec.core import Trace
 from pfs.ga.pfsspec.core.util.args import *
 
@@ -23,10 +23,11 @@ class RVFitTrace(Trace):
                          plot_level=plot_level,
                          log_level=log_level)
         
-        self.plot_rv_prior = False
+        self.plot_priors = False
         self.plot_rv_guess = False
+        self.plot_rv_fit = False
         self.plot_input_spec = False
-        self.plot_fit_spec = False
+        self.plot_fit_spec = {}
         self.plot_spec_flux_err = False
         self.plot_spec_mask = False
         self.plot_spec_cont = False
@@ -45,13 +46,17 @@ class RVFitTrace(Trace):
 
         self.rv_iter = None                    # Keep track of convergence
 
+        self.rv_guess = None
+
     def add_args(self, config, parser):
         super().add_args(config, parser)
     
     def init_from_args(self, script, config, args):
         super().init_from_args(script, config, args)
 
-        self.plot_rv_prior = get_arg('plot_rv_prior', self.plot_rv_prior, args)
+        self.plot_priors = get_arg('plot_priors', self.plot_priors, args)
+        self.plot_rv_guess = get_arg('plot_rv_guess', self.plot_rv_guess, args)
+        self.plot_rv_fit = get_arg('plot_rv_fit', self.plot_rv_fit, args)
         self.plot_input_spec = get_arg('plot_input_spec', self.plot_input_spec, args)
         self.plot_fit_spec = get_arg('plot_fit_spec', self.plot_fit_spec, args)
         self.plot_spec_flux_err = get_arg('plot_spec_flux_err', self.plot_spec_flux_err, args)
@@ -71,7 +76,18 @@ class RVFitTrace(Trace):
             self._plot_spectra('rvfit_input', spectra)
             self.flush_figures()
         
-        # TODO: plot rv_0, prior and bounds here
+        if self.plot_priors:
+            self._plot_prior('rvfit_rv_prior', rv_0, rv_bounds, rv_prior, rv_step)
+            self.flush_figures()
+
+    def on_guess_rv(self, rv, log_L, rv_guess, log_L_fit, function, pp, pcov):
+        # Called when the RV guess is made
+
+        # Store the results for plotting when fitting is done
+        self.guess_rv_results = (rv, log_L, rv_guess, log_L_fit, function, pp, pcov)
+
+        if self.plot_rv_guess or self.plot_level >= Trace.PLOT_LEVEL_INFO:
+            self._plot_rv_guess('rvfit_rv_guess')
     
     def on_fit_rv_iter(self, rv):
         self.rv_iter.append(rv)
@@ -83,14 +99,16 @@ class RVFitTrace(Trace):
         self.rv_iter.append(rv_fit)
         
         # TODO: plot whatever requested
-        if self.plot_fit_spec:
-            self._plot_spectra('rvfit_best', spectra, templates=templates, processed_templates=processed_templates,
-                               plot_spectra=True, plot_processed_templates=True,
-                               plot_flux_err=True, plot_residuals=False)
-            self._plot_spectra('rvfit_residuals', spectra, templates=templates, processed_templates=processed_templates,
-                               plot_spectra=False, plot_processed_templates=False,
-                               plot_flux_err=False, plot_residuals=True)
-            self.flush_figures()
+        for key, config in self.plot_fit_spec.items():
+            self._plot_spectra(key, spectra, templates=templates, processed_templates=processed_templates,
+                               **config)
+
+        if self.plot_rv_fit:
+            self._plot_rv_guess('rvfit_rv_fit',
+                                rv_0, rv_bounds, rv_prior, rv_step,
+                                rv_fit, rv_err)
+
+        self.flush_figures()
 
     def on_process_spectrum(self, arm, i, spectrum, processed_spectrum):
         if self.plot_level >= Trace.PLOT_LEVEL_TRACE:
@@ -143,16 +161,6 @@ class RVFitTrace(Trace):
     def on_eval_F_mcmc(self, x, log_L):
         pass
 
-    def on_guess_rv(self, rv, log_L, rv_guess, fit, function, pp, pcov):
-        if self.plot_rv_guess or self.plot_level >= Trace.PLOT_LEVEL_INFO:
-            (f, ax) = self.get_page('guess_rv', 1, 1)
-            ax.plot(rv, log_L, '.')
-            if fit is not None:
-                ax.plot(rv, fit, **styles.solid_line())
-            ax.axvline(rv_guess, c='r')
-
-            self.flush_figures()
-
     #endregion
 
     def save_history(self, filename, spectrum):
@@ -166,6 +174,7 @@ class RVFitTrace(Trace):
                 f.writelines([ s + '\n' for s in spectrum.history ])
                 
     def _plot_spectra(self, key, spectra, templates=None, processed_templates=None,
+                      wlim=None,
                       plot_spectra=True, plot_flux_err=True,
                       plot_templates=True,
                       plot_processed_templates=True,
@@ -194,25 +203,68 @@ class RVFitTrace(Trace):
 
                 # TODO: define arm color in styles
                 if plot_spectra:
-                    p.plot_spectrum(specs[i], plot_flux_err=plot_flux_err)
+                    p.plot_spectrum(specs[i], plot_flux_err=plot_flux_err, wlim=wlim)
 
                 if plot_templates and templates is not None:
                     raise NotImplementedError()
 
                 if plot_processed_templates and processed_templates is not None:
                     temp = processed_templates[arm][i]
-                    p.plot_template(temp)
+                    p.plot_template(temp, wlim=wlim)
 
                 if plot_residuals and processed_templates is not None:
                     temp = processed_templates[arm][i]
-                    p.plot_residual(spec, temp)
+                    p.plot_residual(spec, temp, wlim=wlim)
 
             # TODO: Add SNR, exp time, obs date
             p.title = spec.get_id_string()
+
             p.apply()
 
         f.match_limits()
 
         pass
 
-    
+    def _plot_prior(self, key, param_0, param_bounds, param_prior, param_step):
+        f = self.get_diagram_page(key, npages=1, nrows=1, ncols=1, page_size=(5.5, 3.5))
+        p = PriorPlot()
+        ax = f.add_diagram((0, 0, 0), p)
+
+        p.plot_prior(param_0, param_bounds, param_prior, param_step)
+
+    def _plot_rv_guess(self, key,
+                       rv_0=None, rv_bounds=None, rv_prior=None, rv_step=None,
+                       rv_fit=None, rv_err=None):
+        
+        # Plot the RV prior, initial value, guess and final fitted value
+
+        f = self.get_diagram_page(key, npages=1, nrows=1, ncols=1, page_size=(5.5, 3.5))
+        ax = f.add_axes((0, 0, 0))
+
+        # Plot RV guess results
+        if self.guess_rv_results is not None and self.plot_rv_guess:
+            (rv, log_L, rv_guess, fit, function, pp, pcov) = self.guess_rv_results
+
+            m = ~np.isnan(log_L) & ~np.isinf(log_L)
+            a = np.nanmax(log_L[m])
+            b = np.nanmin(log_L[m])
+            ax.plot(rv, (log_L - b) / (a - b), '.')
+            if fit is not None:
+                ax.plot(rv, (fit - b) / (a - b), **styles.solid_line())
+            ax.axvline(rv_guess, **styles.blue_line(**styles.solid_line()))
+
+        # Plot the prior on RV
+        if self.plot_priors:
+            p = PriorPlot(ax)
+            ax = f.add_diagram((0, 0, 0), p)
+            p.plot_prior(rv_0, rv_bounds, rv_prior, rv_step, normalize=True)
+
+        # Plot fit results
+        if self.plot_rv_fit and rv_fit is not None:
+            ax.axvline(rv_fit, **styles.red_line(**styles.solid_line()))
+
+            if rv_err is not None:
+                ax.axvline(rv_fit - 3 * rv_err, **styles.red_line(**styles.dashed_line()))
+                ax.axvline(rv_fit + 3 * rv_err, **styles.red_line(**styles.dashed_line()))
+
+        self.flush_figures()
